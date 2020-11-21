a7c18882f05b581e8e1a796fe7bd2178276c9c40
Son Roy Almerol <sonroya.almerol@gmail.com>
2020-11-22 02:50:58 +0800

    add policy and terms

---
+++ components/CustomPage.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+
+import WPGBlocks from 'react-gutenberg';
+
+import { format } from 'date-fns';
+
+import {
+  Typography,
+  Hidden,
+} from '@material-ui/core';
+
+const useStyles = makeStyles(() => ({
+  account: {
+    position: 'absolute',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    textAlign: 'center',
+    right: 0,
+    marginRight: 20,
+    height: 65,
+  },
+  avatar: {
+    height: 60,
+    width: 60,
+  },
+  readMore: {
+    position: 'fixed',
+    width: 'calc(15vw - 10px)',
+    top: 'calc(65px + 8vh)',
+    right: 10,
+  },
+  sideWriter: {
+    position: 'fixed',
+    width: 'calc(15vw - 10px)',
+    top: 'calc(35px + 8vh)',
+    right: 40,
+  },
+}));
+
+export default function Page({ page }) {
+  const classes = useStyles();
+  const theme = useTheme();
+
+  return (
+    <div className={classes.container}>
+      <Hidden smDown>
+        <Typography variant="h3" component="h1" dangerouslySetInnerHTML={{ __html: page.title.rendered }} />
+      </Hidden>
+      <Hidden mdUp>
+        <Typography variant="h4" component="h1" dangerouslySetInnerHTML={{ __html: page.title.rendered }} />
+      </Hidden>
+      <Typography variant="body2" style={{ marginTop: theme.spacing(1) }}>{format(new Date(page.date), 'MMMM d, yyyy')}</Typography>
+      <Typography
+        variant="body1"
+        component="div"
+        style={{
+          marginTop: theme.spacing(2),
+          lineHeight: '1.9em',
+          width: '100%',
+          color: theme.palette.text.primary,
+        }}
+      >
+        <WPGBlocks blocks={page.blocks} />
+      </Typography>
+    </div>
+  );
+}
---
+++ pages/privacy-policy.jsx
@@ -0,0 +1,96 @@
+import React from 'react';
+
+import { NextSeo } from 'next-seo';
+import { useRouter } from 'next/router';
+import DefaultErrorPage from 'next/error';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+
+import WP from '@/utils/wordpress';
+
+import ReactHtmlParser from 'react-html-parser';
+import CustomPage from '@/components/CustomPage';
+
+import { Grid, CircularProgress } from '@material-ui/core';
+
+import { useAuth } from '@/utils/hooks/useAuth';
+
+const useStyles = makeStyles(() => ({
+  contentContainer: {
+    width: '90%',
+    margin: 'auto',
+  },
+}));
+
+export default function Page({ page }) {
+  const classes = useStyles();
+  const theme = useTheme();
+  const { loadingAuth } = useAuth();
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return (
+      <div className={classes.contentContainer}>
+        <Grid container direction="row" justify="center">
+          <CircularProgress style={{ marginTop: 100, marginBottom: 100 }} />
+        </Grid>
+      </div>
+    );
+  }
+
+  if (!page) {
+    return (
+      <DefaultErrorPage statusCode={404} />
+    );
+  }
+
+  return (
+    <div className={classes.container}>
+      <NextSeo
+        title={`${ReactHtmlParser(page.title.rendered)} - Atenews`}
+        description={page.excerpt.rendered.replace(/<[^>]+>/g, '')}
+        openGraph={{
+          title: `${ReactHtmlParser(page.title.rendered)} - Atenews`,
+          description: page.excerpt.rendered.replace(/<[^>]+>/g, ''),
+          images: [
+            {
+              url: '/default-thumbnail.jpg',
+            },
+          ],
+          type: 'article',
+        }}
+        twitter={{
+          handle: '@atenews',
+          cardType: 'summary_large_image',
+        }}
+      />
+      { !loadingAuth ? (
+        <CustomPage page={page} />
+      ) : (
+        <Grid
+          container
+          spacing={0}
+          alignItems="center"
+          justify="center"
+          style={{ minHeight: '100vh' }}
+        >
+          <Grid item>
+            <img src={theme.palette.type === 'light' ? '/logo-blue.png' : '/logo.png'} alt="Atenews Logo" width="100" />
+          </Grid>
+        </Grid>
+      ) }
+    </div>
+  );
+}
+
+export const getStaticProps = async () => {
+  let res = [];
+  try {
+    res = await WP.pages().slug('privacy-policy');
+  } catch (err) {
+    res = [];
+  }
+  if (res.length > 0) {
+    return { props: { page: res[0] }, revalidate: 10 };
+  }
+  return { props: { page: {} }, revalidate: 10 };
+};
