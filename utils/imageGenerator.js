00b0c2d9eb1a9096eba3c98c5474d30b68e09a65
Son Roy Almerol <sonroya.almerol@gmail.com>
2020-11-21 15:09:25 +0800

    catch error for image optimization

--- utils/imageGenerator.js
+++ utils/imageGenerator.js
@@ -1,6 +1,13 @@
 const imageGenerator = (url, width) => {
-  const urlObject = new URL(url);
-  return `https://cdn.statically.io/img/${urlObject.hostname}/w=${width},f=auto${urlObject.pathname}`;
+  if (!url) {
+    return '';
+  }
+  try {
+    const urlObject = new URL(url);
+    return `https://cdn.statically.io/img/${urlObject.hostname}/w=${width},f=auto${urlObject.pathname}`;
+  } catch (err) {
+    return '';
+  }
 };
 
 export default imageGenerator;
