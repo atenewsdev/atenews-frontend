4a1b58a354b0598f84c87c931108f60d11c52b1a
Son Roy Almerol <sonroya.almerol@gmail.com>
2020-11-22 00:48:48 +0800

    add headers to error pages

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,29 +1,36 @@
 import React from 'react';
 
+import Head from 'next/head';
+
 import { Grid, Typography } from '@material-ui/core';
 
 export default function Custom404() {
   return (
-    <Grid
-      container
-      direction="column"
-      spacing={0}
-      alignItems="center"
-      justify="center"
-      style={{ minHeight: '50vh' }}
-    >
-      <Grid item>
-        <img src="/reacts/sad.svg" alt="sad" width={100} />
+    <>
+      <Head>
+        <title>Error 404 - Atenews</title>
+      </Head>
+      <Grid
+        container
+        direction="column"
+        spacing={0}
+        alignItems="center"
+        justify="center"
+        style={{ minHeight: '50vh' }}
+      >
+        <Grid item>
+          <img src="/reacts/sad.svg" alt="sad" width={100} />
+        </Grid>
+        <Grid item>
+          <Typography variant="h4">Error 404</Typography>
+        </Grid>
+        <Grid item>
+          <Typography variant="h5">Page not found!</Typography>
+        </Grid>
+        <Grid item>
+          <Typography variant="body1">Sorry for the inconvenience 👉👈</Typography>
+        </Grid>
       </Grid>
-      <Grid item>
-        <Typography variant="h4">Error 404</Typography>
-      </Grid>
-      <Grid item>
-        <Typography variant="h5">Page not found!</Typography>
-      </Grid>
-      <Grid item>
-        <Typography variant="body1">Sorry for the inconvenience 👉👈</Typography>
-      </Grid>
-    </Grid>
+    </>
   );
 }
--- pages/_error.jsx
+++ pages/_error.jsx
@@ -1,36 +1,43 @@
 import React from 'react';
 
+import Head from 'next/head';
+
 import { Grid, Typography } from '@material-ui/core';
 
 const CustomError = ({ statusCode }) => (
-  <Grid
-    container
-    direction="column"
-    spacing={0}
-    alignItems="center"
-    justify="center"
-    style={{ minHeight: '50vh' }}
-  >
-    <Grid item>
-      <img src="/reacts/sad.svg" alt="sad" width={100} />
+  <>
+    <Head>
+      <title>{`Error ${statusCode} - Atenews`}</title>
+    </Head>
+    <Grid
+      container
+      direction="column"
+      spacing={0}
+      alignItems="center"
+      justify="center"
+      style={{ minHeight: '50vh' }}
+    >
+      <Grid item>
+        <img src="/reacts/sad.svg" alt="sad" width={100} />
+      </Grid>
+      <Grid item>
+        {statusCode ? (
+          <Typography variant="h4">{`Error ${statusCode}`}</Typography>
+        ) : (
+          <Typography variant="h4">An error occured on client!</Typography>
+        )}
+      </Grid>
+      <Grid item>
+        <Typography variant="h5">The dev team is working hard to fix this!</Typography>
+      </Grid>
+      <Grid item>
+        <Typography variant="body1">Try to refresh the page in a few moments.</Typography>
+      </Grid>
+      <Grid item>
+        <Typography variant="body1">Sorry for the inconvenience 👉👈</Typography>
+      </Grid>
     </Grid>
-    <Grid item>
-      {statusCode ? (
-        <Typography variant="h4">{`Error ${statusCode}`}</Typography>
-      ) : (
-        <Typography variant="h4">An error occured on client!</Typography>
-      )}
-    </Grid>
-    <Grid item>
-      <Typography variant="h5">The dev team is working hard to fix this!</Typography>
-    </Grid>
-    <Grid item>
-      <Typography variant="body1">Try to refresh the page in a few moments.</Typography>
-    </Grid>
-    <Grid item>
-      <Typography variant="body1">Sorry for the inconvenience 👉👈</Typography>
-    </Grid>
-  </Grid>
+  </>
 );
 
 CustomError.getInitialProps = ({ res, err }) => {
