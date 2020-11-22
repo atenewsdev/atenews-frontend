1f0b23f4c02a1566b5cbe3f1af1b5c438832733a
Son Roy Almerol <sonroya.almerol@gmail.com>
2020-11-22 22:00:08 +0800

    fix trending bug

--- utils/hooks/useTrending.jsx
+++ utils/hooks/useTrending.jsx
@@ -10,22 +10,23 @@ export const TrendingProvider = ({ children }) => {
   const [trending, setTrending] = useState([]);
 
   useEffect(() => {
-    const unsubscribe = firebase.database().ref('articles').orderByChild('trendScore').limitToLast(5)
+    const unsubscribe = firebase.database().ref('articles').orderByChild('trendScore').limitToLast(10)
       .on('value', (doc) => {
         if (doc) {
           const values = doc.val();
-          setTrending(Object.keys(values).map((key) => {
+          const result = [];
+          Object.keys(values).forEach((key) => {
             const obj = values[key];
             const backupCat = obj.categories;
             if (obj.categories) {
               obj.categories = Object.keys(backupCat).map((keyCat) => backupCat[keyCat]);
             }
             obj.slug = key;
-            if (obj.trashed) {
-              return null;
+            if (!obj.trashed) {
+              result.push(obj);
             }
-            return obj;
-          }).sort((a, b) => b.trendScore - a.trendScore));
+          });
+          setTrending(result.sort((a, b) => b.trendScore - a.trendScore).slice(0, 5));
         }
       });
     return () => {
