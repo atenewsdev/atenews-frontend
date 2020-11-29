ee5f4e0b93284436a09052256350ab76dff99e99
Son Roy Almerol <sonroya.almerol@gmail.com>
2020-11-29 17:34:52 +0800

    add more icons

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -20,6 +20,7 @@ export default class MyDocument extends Document {
           <meta name="format-detection" content="telephone=no" />
           <meta name="mobile-web-app-capable" content="yes" />
           <meta name="theme-color" content="#195EA9" />
+          <meta name="msapplication-TileColor" content="#195EA9" />
 
           <link rel="preconnect" href="https://fonts.gstatic.com" />
 
@@ -29,7 +30,23 @@ export default class MyDocument extends Document {
           <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700;800&display=swap" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800;900&display=swap" />
 
+          <link rel="apple-touch-icon" sizes="57x57" href="/icons/apple-icon-57x57.png" />
+          <link rel="apple-touch-icon" sizes="60x60" href="/icons/apple-icon-60x60.png" />
+          <link rel="apple-touch-icon" sizes="72x72" href="/icons/apple-icon-72x72.png" />
+          <link rel="apple-touch-icon" sizes="76x76" href="/icons/apple-icon-76x76.png" />
+          <link rel="apple-touch-icon" sizes="114x114" href="/icons/apple-icon-114x114.png" />
+          <link rel="apple-touch-icon" sizes="120x120" href="/icons/apple-icon-120x120.png" />
+          <link rel="apple-touch-icon" sizes="144x144" href="/icons/apple-icon-144x144.png" />
+          <link rel="apple-touch-icon" sizes="152x152" href="/icons/apple-icon-152x152.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-icon-180x180.png" />
+          <link rel="icon" type="image/png" sizes="192x192" href="/icons/android-icon-192x192.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="96x96" href="/icons/favicon-96x96.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+
           <link rel="manifest" href="/manifest.json" />
+
+          <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
         </Head>
         <body>
           <Main />
