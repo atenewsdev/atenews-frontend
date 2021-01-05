56e857f9415f53bbf2bfeab3d9178259b6f40d21
Son Roy Almerol <sonroya.almerol@gmail.com>
2021-01-05 17:11:55 +0800

    fix oobcode

--- pages/auth/action.jsx
+++ pages/auth/action.jsx
@@ -13,36 +13,38 @@ const ActionAuth = () => {
 
   const { setSuccess, setError } = useError();
   React.useEffect(() => {
-    switch (router.query.mode) {
-      case 'resetPassword':
-        // Display reset password handler and UI.
-        setError('Reset password has not been implemented yet!');
-        router.push('/');
-        break;
-      case 'recoverEmail':
-        // Display email recovery handler and UI.
-        setError('Email recovery has not been implemented yet!');
-        router.push('/');
-        break;
-      case 'verifyEmail':
-        // Display email verification handler and UI.
-        firebase.auth().applyActionCode(router.query.oobCode).then(() => {
-          setSuccess('Successfully verified email!');
-          if (router.query.continueUrl) {
-            router.push(router.query.continueUrl);
-          } else {
-            router.push('/');
-          }
-        }).catch((err) => {
-          setError(err.message);
+    if (router.query.oobCode) {
+      switch (router.query.mode) {
+        case 'resetPassword':
+          // Display reset password handler and UI.
+          setError('Reset password has not been implemented yet!');
           router.push('/');
-        });
-        break;
-      default:
-        // Error: invalid mode.
-        break;
+          break;
+        case 'recoverEmail':
+          // Display email recovery handler and UI.
+          setError('Email recovery has not been implemented yet!');
+          router.push('/');
+          break;
+        case 'verifyEmail':
+          // Display email verification handler and UI.
+          firebase.auth().applyActionCode(router.query.oobCode).then(() => {
+            setSuccess('Successfully verified email!');
+            if (router.query.continueUrl) {
+              router.push(router.query.continueUrl);
+            } else {
+              router.push('/');
+            }
+          }).catch((err) => {
+            setError(err.message);
+            router.push('/');
+          });
+          break;
+        default:
+          // Error: invalid mode.
+          break;
+      }
     }
-  }, [router.query.mode, router.query.oobCode]);
+  }, [router.query.oobCode]);
 
   return (
     <Grid container justify="center" alignItems="center" spacing={2}>
--- pages/index.jsx
+++ pages/index.jsx
@@ -71,32 +71,34 @@ export default function Home({
 
   const { setSuccess, setError } = useError();
   React.useEffect(() => {
-    switch (mode) {
-      case 'resetPassword':
-        // Display reset password handler and UI.
-        setError('Reset password has not been implemented yet! Please contact us at dev@atenews.ph for urgent matters.');
-        break;
-      case 'recoverEmail':
-        // Display email recovery handler and UI.
-        setError('Email recovery has not been implemented yet! Please contact us at dev@atenews.ph for urgent matters.');
-        break;
-      case 'verifyEmail':
-        // Display email verification handler and UI.
-        firebase.auth().applyActionCode(oobCode).then(() => {
-          firebase.auth().currentUser.reload();
-          setSuccess('Successfully verified email!');
-          if (continueUrl) {
-            router.push(continueUrl);
-          }
-        }).catch((err) => {
-          setError(err.message);
-        });
-        break;
-      default:
-        // Error: invalid mode.
-        break;
+    if (oobCode) {
+      switch (mode) {
+        case 'resetPassword':
+          // Display reset password handler and UI.
+          setError('Reset password has not been implemented yet! Please contact us at dev@atenews.ph for urgent matters.');
+          break;
+        case 'recoverEmail':
+          // Display email recovery handler and UI.
+          setError('Email recovery has not been implemented yet! Please contact us at dev@atenews.ph for urgent matters.');
+          break;
+        case 'verifyEmail':
+          // Display email verification handler and UI.
+          firebase.auth().applyActionCode(oobCode).then(() => {
+            firebase.auth().currentUser.reload();
+            setSuccess('Successfully verified email!');
+            if (continueUrl) {
+              router.push(continueUrl);
+            }
+          }).catch((err) => {
+            setError(err.message);
+          });
+          break;
+        default:
+          // Error: invalid mode.
+          break;
+      }
     }
-  }, [mode]);
+  }, [oobCode]);
 
   return (
     <div className={classes.container}>
