9fe466c6dd97f661f34c730f713135f9bf50ebaf
Son Roy Almerol <sonroya.almerol@gmail.com>
2021-01-12 02:34:08 +0800

    add error log

--- pages/api/auth/verify.js
+++ pages/api/auth/verify.js
@@ -9,7 +9,7 @@ export default async (req, res) => {
       await verifyEmail(email);
       res.status(200).json({ message: 'success' });
     } catch (err) {
-      res.status(500).json({ ...err, email });
+      res.status(500).json({ message: err.message });
     }
   } else {
     res.status(404).send({ message: 'not found' });
