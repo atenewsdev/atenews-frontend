1a1e3f94eeb30a5be801f18372e04317c744ba4d
Son Roy Almerol <sonroya.almerol@gmail.com>
2021-01-17 20:34:49 +0800

    add preconnect statically

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -24,6 +24,7 @@ export default class MyDocument extends Document {
 
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link rel="preconnect" href="https://firebase.googleapis.com" />
+          <link rel="preconnect" href="https://cdn.statically.io" />
 
           <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700;800&display=swap" />
           <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800;900&display=swap" />
