49626307273328030c4726ae5f4f0ac448c9740d
Son Roy Almerol <sonroya.almerol@gmail.com>
2021-01-22 14:43:00 +0800

    clean up custom error

--- pages/_error.jsx
+++ pages/_error.jsx
@@ -1,30 +1,9 @@
 import React from 'react';
 
 import Error from '@/components/Error';
-/* React.useEffect(() => {
-    const params = {
-      error,
-    };
 
-    let formBody = [];
-    // eslint-disable-next-line guard-for-in
-    for (const property in params) {
-      const encodedKey = encodeURIComponent(property);
-      const encodedValue = encodeURIComponent(params[property]);
-      formBody.push(`${encodedKey}=${encodedValue}`);
-    }
-    formBody = formBody.join('&');
-
-    fetch('/api/bugs/report', {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-      },
-      body: formBody,
-    }).then((response) => response.json());
-  }, []); */
 const CustomError = ({ statusCode }) => <Error statusCode={statusCode} />;
+
 CustomError.getInitialProps = ({ res, err }) => {
   // eslint-disable-next-line no-nested-ternary
   const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
